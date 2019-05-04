51c6676c093219137aeca0698aea43820ffedff6
BNEJesse <491974150@qq.com>
2019-05-04 18:14:20 +1000

    git the first file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import "./styles/main.css";
+import React, { Component } from 'react';
+import './styles/main.css';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import WeatherChannel from "./components/WeatherChannel";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import WeatherChannel from './components/WeatherChannel';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="weather-channel__container">
+      <div className='weather-channel__container'>
         <Header />
         <WeatherChannel />
         <Footer />
@@ -16,3 +16,4 @@ export default class App extends Component {
     );
   }
 }
+//i
